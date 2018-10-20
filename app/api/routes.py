57874ee2a7dff0cd11d6b161b610b39a3a9d85a2
Leon Sandøy <leon.sandoy@conmodo.com>
2018-10-21 00:08:07 +0200

    Implemented a case insensitive optional language or category filter for the resources API endpoint.

--- app/api/routes.py
+++ app/api/routes.py
@@ -2,17 +2,17 @@ from traceback import print_tb
 
 from flask import request
 from flask import jsonify
+from sqlalchemy import and_, func
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 
 from app.api import bp
-from app.models import Language, Resource
+from app.models import Language, Resource, Category
 from app import Config
 from app.utils import Paginator
 
 # Routes
 @bp.route('/resources', methods=['GET'])
 def resources():
-    print("feinsmecker")
     return get_resources()
 
 
@@ -48,18 +48,56 @@ def get_resource(id):
 
 
 def get_resources():
-    try:
-        resource_paginator = Paginator(Config.RESOURCE_PAGINATOR, Resource, request)
-        resource_list = [resource.serialize for resource in resource_paginator.items]
+    """
+    Gets a paginated list of resources.
 
-        lang = request.args.get('lang')
+    If the URL parameters `language` or `category` are found
+    in the request, the list will be filtered by these parameters.
+
+    The filters are case insensitive.
+    """
+    resource_list = []
+    try:
+        resource_paginator = Paginator(Config.RESOURCE_PAGINATOR, request)
+
+        # Fetch the filter params from the url, if they were provided.
+        language = request.args.get('language')
         category = request.args.get('category')
 
-        # Filter out hits that don't match the filter params.
-        if lang:
-            resource_list = [resource for resource in resource_list if lang in resource.serialize_languages]
-        if category:
-            resource_list = [resource for resource in resource_list if category == resource.category.name]
+        # Filter on language
+        if language and not category:
+            query = Resource.query.filter(
+                Resource.languages.any(
+                    Language.name.ilike(language)
+                )
+            )
+
+        # Filter on category
+        elif category and not language:
+            query = Resource.query.filter(
+                Resource.category.has(
+                    func.lower(Category.name) == category.lower()
+                )
+            )
+
+        # Filter on both
+        elif category and language:
+            query = Resource.query.filter(
+                and_(
+                    Resource.languages.any(
+                        Language.name.ilike(language)
+                    ),
+                    Resource.category.has(
+                        func.lower(Category.name) == category.lower()
+                    )
+                )
+            )
+
+        # No filters
+        else:
+            query = Resource.query
+
+        resource_list = [resource.serialize for resource in resource_paginator.items(query)]
 
     except Exception as e:
         print_tb(e.__traceback__)
