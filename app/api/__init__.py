0400204d6551943253d00126fedc84ae2f1f2d55
Leon Sandøy <leon.sandoy@conmodo.com>
2018-10-20 21:49:41 +0200

    Fixes the route registration bug introduced by the linting merge

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,3 +1,6 @@
 from flask import Blueprint
 
 bp = Blueprint('api', __name__)
+
+# We need to import the routes here so they will bind to the blueprint before the blueprint is registered.
+from app.api import routes  # noqa
