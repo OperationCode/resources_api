e1433b208f90e1886372421265e3544b865e0ecc
wimo7083 <wimo7083@colorado.edu>
2018-10-23 15:36:42 -0600

    initial tests

---
+++ tests/unit/test_routes.py
@@ -0,0 +1,10 @@
+from app.models import Resource, Language, Category
+
+
+def test_does_nothing():
+    """
+    GIVEN a User model
+    WHEN a new User is created
+    THEN check the email, hashed_password, authenticated, and role fields are defined correctly
+    """
+    assert(1 == 1)
