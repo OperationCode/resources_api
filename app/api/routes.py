c704cfa095e88d1e4033d911ddab7df957879be1
michizhou <myz227@nyu.edu>
2018-10-24 22:29:06 -0400

    Modified implementation for creating new resource

--- app/api/routes.py
+++ app/api/routes.py
@@ -2,11 +2,12 @@ from traceback import print_tb
 
 from flask import request
 from flask import jsonify
+from sqlalchemy import exc, and_, func
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 
 from app.api import bp
-from app.models import Language, Resource
-from app import Config
+from app.models import Language, Resource, Category
+from app import Config, db
 from app.utils import Paginator
 
 
@@ -16,34 +17,19 @@ def resources():
     return get_resources()
 
 
-@bp.route('/resources/<int:id>', methods=['GET'])
-def resource_get(id):
-    return get_resource(id)
-
-
-@bp.route('/resources/<int:id>', methods=['GET', 'POST', 'PUT'])
-def resource(id, params="", category=None, languages=[], name=None, url=None,
-                 paid=False, notes=None):
+@bp.route('/resources/<int:id>', methods=['GET', 'PUT', 'POST'])
+def resource(id):
     if request.method == 'GET':
         return get_resource(id)
     elif request.method == 'PUT':
-        param_list = [category, languages, name, url, paid, notes]
-        param_names = ['category', 'languages', 'name', 'url', 'paid', 'notes']
-        for index in range(len(param_names)):
-            if request.args.get(param_names[index]):
-                param_list[index] = request.args.get(param_names[index])
-        return set_resource(id, param_list)
+        return set_resource(id, request.get_json(), db)
     elif request.method == 'POST':
-        new_resource = Resource()
-        new_resource.category = request.args.get('category')
-        new_resource.languages = request.args.get('languages')
-        new_resource.name = request.args.get('name')
-        new_resource.url = request.args.get('url')
-        if request.args.get('paid'):
-            new_resource.paid = request.args.get('paid')
-        if request.args.get('notes'):
-            new_resource.notes = request.args.get('notes')
-        return jsonify(new_resource.serialize)
+        return create_resource(request.get_json(), db)
+
+
+@bp.route('/languages', methods=['GET'])
+def languages():
+    return get_languages()
 
 
 # Helpers
@@ -68,9 +54,57 @@ def get_resource(id):
 
 
 def get_resources():
+    """
+    Gets a paginated list of resources.
+
+    If the URL parameters `language` or `category` are found
+    in the request, the list will be filtered by these parameters.
+
+    The filters are case insensitive.
+    """
     try:
-        resource_paginator = Paginator(Config.RESOURCE_PAGINATOR, Resource, request)
-        resource_list = [resource.serialize for resource in resource_paginator.items]
+        resource_paginator = Paginator(Config.RESOURCE_PAGINATOR, request)
+
+        # Fetch the filter params from the url, if they were provided.
+        language = request.args.get('language')
+        category = request.args.get('category')
+
+        # Filter on language
+        if language and not category:
+            query = Resource.query.filter(
+                Resource.languages.any(
+                    Language.name.ilike(language)
+                )
+            )
+
+        # Filter on category
+        elif category and not language:
+            query = Resource.query.filter(
+                Resource.category.has(
+                    func.lower(Category.name) == category.lower()
+                )
+            )
+
+        # Filter on both
+        elif category and language:
+            query = Resource.query.filter(
+                and_(
+                    Resource.languages.any(
+                        Language.name.ilike(language)
+                    ),
+                    Resource.category.has(
+                        func.lower(Category.name) == category.lower()
+                    )
+                )
+            )
+
+        # No filters
+        else:
+            query = Resource.query
+
+        resource_list = [
+            resource.serialize for resource in resource_paginator.items(query)
+        ]
 
     except Exception as e:
         print_tb(e.__traceback__)
@@ -92,8 +126,25 @@ def get_languages():
     finally:
         return jsonify(language_list)
 
+        
+def get_attributes():
+    languages_list = Language.query.all()
+    categories_list = Category.query.all()
 
-def set_resource(id, param_list):
+    language_dict = {l.key(): l for l in languages_list}
+    category_dict = {c.key(): c for c in categories_list}
+
+    langs = []
+    for lang in json.get('languages') or []:
+        language = language_dict.get(lang)
+        if not language:
+            language = Language(name=lang)
+        langs.append(language)
+    categ = category_dict.get(json.get('category'), Category(name=json.get('category')))
+    return (langs, categ)
+
+
+def set_resource(id, json, db):
     resource = None
     try:
         resource = Resource.query.get(id)
@@ -108,12 +159,43 @@ def set_resource(id, param_list):
 
     finally:
         if resource:
-            resource.category = param_list[0]
-            resource.languages = param_list[1]
-            resource.name = param_list[2]
-            resource.url = param_list[3]
-            resource.paid = param_list[4]
-            resource.notes = param_list[5]
+            if json.get('languages'):
+                resource.languages = get_attributes()[0]
+            if json.get('category'):
+                resource.category = get_attributes()[1]
+            if json.get('name'):
+                resource.name = json.get('name')
+            if json.get('url'):
+                resource.url = json.get('url')
+            if json.get('paid'):
+                resource.paid = json.get('paid')
+            if json.get('notes'):
+                resource.notes = json.get('notes')
+            try:
+                db.session.commit()
+            except exc.SQLAlchemyError as e:
+                db.session.rollback()
+                print('Flask SQLAlchemy Exception:', e)
+                print(resource)
             return jsonify(resource.serialize)
         else:
             return jsonify({})
+
+
+def create_resource(json, db):
+    new_resource = Resource(
+        name=json.get('name'),
+        url=json.get('url'),
+        category=get_attributes()[1],
+        languages=get_attributes()[0],
+        paid=json.get('paid'),
+        notes=json.get('notes'),
+        upvotes=None,
+        downvotes=None,
+        times_clicked=None)
+
+    try:
+        db.session.add(new_resource)
+    except Exception as e:
+        print('exception', e)
+    return jsonify(new_resource.serialize)
