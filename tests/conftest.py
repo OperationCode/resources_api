e215c70cff0e302f7ef76af5af007a09c0f6c4f9
wimo7083 <wimo7083@colorado.edu>
2018-12-29 14:45:53 -0700

    create db scoped to session for shared data

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,11 +8,10 @@ counter = 0
 
 
 @pytest.fixture(scope='session')
-def app(request):
+def session_app(request):
     Config.SQLALCHEMY_DATABASE_URI = TEST_DATABASE_URI
     Config.TESTING = True
     app = create_app(Config)
-
 
     # Establish an application context before running the tests.
     ctx = app.app_context()
@@ -26,28 +25,28 @@ def app(request):
 
 
 @pytest.fixture(scope='session')
-def db(app, request):
+def session_db(session_app, request):
     """Session-wide test database."""
+
     def teardown():
         _db.drop_all()
 
-    _db.app = app
+    _db.app = session_app
     _db.create_all()
-
     request.addfinalizer(teardown)
     return _db
 
 
 @pytest.fixture(scope='function')
-def session(db, request):
+def function_session(session_db, request):
     """Creates a new database session for a test."""
-    connection = db.engine.connect()
+    connection = session_db.engine.connect()
     transaction = connection.begin()
 
     options = dict(bind=connection, binds={})
-    session = db.create_scoped_session(options=options)
+    session = session_db.create_scoped_session(options=options)
 
-    db.session = session
+    session_db.session = session
 
     def teardown():
         transaction.rollback()
@@ -56,3 +55,33 @@ def session(db, request):
 
     request.addfinalizer(teardown)
     return session
+
+
+@pytest.fixture(scope='module')
+def module_client():
+    Config.SQLALCHEMY_DATABASE_URI = TEST_DATABASE_URI
+    Config.TESTING = True
+    flask_app = create_app(Config)
+
+    # Flask provides a way to test your application by exposing the Werkzeug test Client
+    # and handling the context locals for you.
+    testing_client = flask_app.test_client()
+
+    # Establish an application context before running the tests.
+    ctx = flask_app.app_context()
+    ctx.push()
+
+    yield testing_client  # this is where the testing happens!
+
+    ctx.pop()
+
+
+@pytest.fixture(scope='module')
+def module_db():
+    # Create the database and the database table
+    _db.create_all()
+    from app.cli import import_resources
+    import_resources(_db)
+    yield _db  # this is where the testing happens!
+
+    _db.drop_all()
