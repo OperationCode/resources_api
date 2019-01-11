ecc5c8fa2cacc427c4ce0c4b2e18a10eb308571a
Aaron Suarez <aaron.j.suarez1@gmail.com>
2019-01-10 21:08:45 -0600

    Verify the API key is in the header

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,9 +1,15 @@
 import requests
+from flask import request
+from app.models import Key
+from app.utils import standardize_response
 
 
 def authenticate(func):
-
     def wrapper(*args, **kwargs):
+        apikey = request.headers.get('x-apikey')
+        if not Key.query.filter_by(apikey=apikey).first():
+            errors = [{"code": "not-authorized"}]
+            return standardize_response(None, errors, "not authorized", 401)
         return func(*args, **kwargs)
     return wrapper
 
