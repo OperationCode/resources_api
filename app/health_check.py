84ed578799ef361c2ee19f4dfebf1cf302f9423a
Aaron Suarez <aaron.j.suarez1@gmail.com>
2019-03-17 21:16:34 -0500

    Remove unnecessary healthcheck.py

--- app/health_check.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-
-def health_database_status():
-    try:
-        db.session.query("1").from_statement("SELECT 1").all()
-        return '<h1>It works.</h1>'
-    except Exception:
-        return '<h1>Something is broken.</h1>'
