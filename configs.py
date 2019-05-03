eff8706d076dd52a7d12abe9995a55cfa1d38ef1
Richard <richardjbrown1@gmail.com>
2019-05-03 11:42:31 -0600

    Add /search endpoint

--- configs.py
+++ configs.py
@@ -32,10 +32,22 @@ pg_host = os.environ.get('POSTGRES_HOST')
 if not pg_host:
     raise KeyError("Application requires 'POSTGRES_HOST' to run")
 
+search_user = os.environ.get('SEARCH_USER')
+search_key = os.environ.get('SEARCH_KEY')
+if not all([search_user, search_key]):
+    print("Application requires 'SEARCH_USER' and 'SEARCH_KEY' for search functionality.")
+
+search_resource_index = os.environ.get("SEARCH_RESOURCE_INDEX")
+
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     SQLALCHEMY_DATABASE_URI = f"postgresql://{pg_user}:{pg_pw}@{pg_host}:5432/{pg_db}"
+    SEARCH_USER = search_user
+    SEARCH_KEY = search_key
+    SEARCH_RESOURCE_INDEX = search_resource_index
+
 
     # Can pass in changes to defaults, such as PaginatorConfig(per_page=40)
     RESOURCE_PAGINATOR = PaginatorConfig()
