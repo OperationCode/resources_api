a0da60b6fc2278cd7f3039331fb269e3716651ee
Aaron Suarez <aaron.j.suarez1@gmail.com>
2019-07-27 19:18:27 -0500

    Force browser not to cache old versions of the OpenAPI file

--- app/views/routes.py
+++ app/views/routes.py
@@ -4,13 +4,16 @@ from flask import send_from_directory, current_app, render_template
 
 from app.views import bp
 
+import os
+
 
 @bp.route('/', methods=['GET'])
 def docs():
     """
     Displays the API documentation at '/'
     """
-    return render_template('index.html')
+    return render_template('index.html',
+                           last_updated=dir_last_updated('app/static'))
 
 
 @bp.route('/openapi.yaml', methods=['GET'])
@@ -27,3 +30,13 @@ def favicon():
     Serves the favicon
     """
     return send_from_directory(join(current_app.root_path, 'static'), 'favicon.ico')
+
+
+def dir_last_updated(folder):
+    """
+    Returns the timestamp of the last time the static folder was updated
+    to avoid the browser caching an outdated openapi.yaml file
+    """
+    return str(max(os.path.getmtime(os.path.join(root_path, f))
+               for root_path, dirs, files in os.walk(folder)
+               for f in files))
