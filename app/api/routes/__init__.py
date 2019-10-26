253eabd28956a8ce5085fbf9525b195ec6e0de0a
Joel Gerber <Joel.Gerber@corp.eastlink.ca>
2019-10-26 17:16:26 -0400

    Split api key route into separate module

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -1,4 +1,4 @@
-from app.api.routes import old_routes
+from app.api.routes import api_key
 from app.api.routes import resources
 from app.api.routes import resource_voting
 from app.api.routes import categories
