5c2a6d4c9c7241d28b49ec8432a8dc31c7f93157
Chongyoon Nah <chongyoonaaronnah@gmail.com>
2020-03-12 20:11:34 -0400

    use "request.get_json()" instead of try/except when parsing payload

--- app/api/auth.py
+++ app/api/auth.py
@@ -5,7 +5,6 @@ import requests
 from app.models import Key
 from app.utils import setup_logger, standardize_response
 from flask import g, request
-from werkzeug.exceptions import HTTPException
 
 auth_logger = setup_logger('auth_logger')
 create_logger = setup_logger('create_auth_logger')
@@ -117,9 +116,6 @@ def log_request(request, key):
     method = request.method
     path = request.path
     user = key.email
-    try:
-        payload = request.get_json()
-    except HTTPException:
-        payload = None
+    payload = request.get_json(silent=True)
     logger = create_logger if method == "POST" else update_logger
     logger.info(f"User: {user} Route: {path} Payload: {payload}")
